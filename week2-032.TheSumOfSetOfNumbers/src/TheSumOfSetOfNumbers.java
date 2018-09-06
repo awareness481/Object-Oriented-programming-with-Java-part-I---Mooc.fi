59cd90d0a6b0871d1cba19f0902107aa2f98b7e6
Jesse Jafa <jesse.jafa@gmail.com>
2018-09-06 11:51:30 +0300

    032

---
+++ week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class TheSumOfSetOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Until what? ");
+        int n = Integer.parseInt(reader.nextLine());
+        
+        int i = 1;
+        int sum = 0;
+        while(i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum is " + sum);
+    }
+}
