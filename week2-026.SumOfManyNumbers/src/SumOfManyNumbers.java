b8845304317ebb74ce10bc761680073081b0afe3
Jesse Jafa <jesse.jafa@gmail.com>
2018-09-06 11:50:03 +0300

    026

---
+++ week2-026.SumOfManyNumbers/src/SumOfManyNumbers.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+
+public class SumOfManyNumbers {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        
+        while (true) {
+            System.out.print("Type a number: ");
+            int read = Integer.parseInt(reader.nextLine());
+            sum += read;
+            
+            if (read == 0) {
+                break;
+            }
+
+            // do something here
+
+            System.out.println("Sum now: " + sum);
+        }
+        
+        System.out.println("Sum in the end: " + sum);
+    }
+}
