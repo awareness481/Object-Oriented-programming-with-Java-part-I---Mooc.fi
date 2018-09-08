132458c21aee665526593fe8ed67ac00400762b3
Jesse Jafa <jesse.jafa@gmail.com>
2018-09-08 14:53:56 +0300

    085

---
+++ week5-085.Reformatory/src/Reformatory.java
@@ -0,0 +1,18 @@
+public class Reformatory {
+    private int count = 0;
+
+    public int weight(Person person) {
+        // return the weight of the person
+        this.count++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        person.setWeight(this.weight(person) + 1);
+    }
+    
+    public int totalWeightsMeasured() {
+        return this.count;
+    }
+
+}
